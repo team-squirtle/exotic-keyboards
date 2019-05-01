9ea1709dc19eaacbb3af0b99addd021f44bb9545
Jonathan Peralez <jonathanperalez@Jonathans-MacBook-Pro-2.local>
2019-05-01 15:08:51 -0700

    user login in process

--- server/app.js
+++ server/app.js
@@ -10,27 +10,24 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 //routes
-
 const productRoute = require('./routes/productRoute.js');
 const userRoute = require('./routes/userRoutes.js');
-
-app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-
-// app.use('/build', express.static(path.join(__dirname, '../build')));
+//app.use('/build', express.static(path.join(__dirname, '../build')));
 app.use('/api/product', productRoute);
 app.use('/api/users', userRoute);
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
  res.sendFile(path.join(__dirname, '../index.html'));
  //  res.send('reached root route');
 });
 
+app.get('*', (req, res) => {
+  res.status(404).send('unknown');
+});
 
 module.exports = app;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/', (req, res) => {
-  // res.send('reached')
+
 });
 
 router.post('/', (req, res) => {
-    // res.send('reached me too')
-  // userController.createUser(req, res);
   userController.createUser(req, res);
+});
+
+router.post('/login', (req, res) => {
+
 });
 
 module.exports = router;
